7918c4572ad9a1a8e6f1f164393035902360d2bf
Dilliram0 <bradjohn235@gmail.com>
2026-01-18 08:35:31 +0000

    ToDo App

---
+++ Day Ten/18_ToDoList/script.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('form');
+const taskList = document.querySelector('#task-list');
+const deleteBtn = document.querySelector('#delete')
+
+function addTask(task){
+    let listItem = document.createElement('li');
+    listItem.innerHTML = `<input type="checkbox" /> <span> ${task} </span> <button> Delete </button>`;
+    taskList.appendChild(listItem);
+}
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const input = document.querySelector('#input-task');
+    const task = input.value;
+    addTask(task);
+    input.value = "";
+})
+
+taskList.addEventListener('click', (e)=> {
+    if(e.target.tagName === 'BUTTON'){
+        const listItem = e.target.parentElement;
+        taskList.removeChild(listItem);
+        console.log(e.target.parentElement)
+    }
+    console.log(e.target.tagName);
+})
